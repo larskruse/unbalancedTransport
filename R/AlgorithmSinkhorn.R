a7d5412219030cdd07cd2ed87614ef751ec93e55
lars.kruse <lars.kruse@stud.uni-goettingen.de>
2021-06-01 02:05:15 +0200

    break if change in dual variables in smaller than <tol>

--- R/AlgorithmSinkhorn.R
+++ R/AlgorithmSinkhorn.R
@@ -17,7 +17,7 @@
 #' @export
 #'
 sinkhornAlgorithmFromCost <- function(costMatrix, supplyList, demandList,
-                                     maxIteration, eps, tol = 1e-10){
+                                     maxIteration, eps, tol = 1e-3){
     
     supplyReg <- 0
     demandReg <- 0
